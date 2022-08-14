cd14cb190e8bcae5862451108ce031308198f04b
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-14 15:44:43 -0300

    Importacion de estilos css.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img class="fade-in" alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
