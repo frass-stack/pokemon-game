cabea1c04fbffd9389625704ca4d2344853ff83a
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-17 00:06:42 -0300

    Diseño de componentes PokemonPicture y PokemonOptions

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,13 @@
 <template>
-  <h2>Pokemon Picture</h2>
+  <div class="pokemon-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      class="hidden-pokemon"
+      alt="pokemon">
+
+    <!-- <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      class="fade-in"
+      alt="pokemon"> -->
+  </div>
 </template>
 
 <script>
@@ -8,6 +16,24 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 32%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+
+.hidden-pokemon {
+  filter:brightness(0)
+}
 
 </style>
